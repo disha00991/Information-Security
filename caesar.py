33c4361847582726395bb581c0d19f604e772b92
ashu01 <as01hu@gmail.com>
2016-08-26 19:56:57 +0530

    change

--- caesar.py
+++ caesar.py
@@ -2,7 +2,7 @@
 def caesar(plaintext,shift):
 
 	alphabet=["a","b","c","d","e","f","g","h","i","j","k","l",
-	"m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
+	"m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 	#Create our substitution dictionary
 	dic={}
